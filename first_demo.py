b60a20d2a5f0c0985de3873534387e7d8185efc5
Beckenbear120 <maximaxi777@qq.com>
2026-09-13 18:36:59 +0800

    feat: 前端UI项目完整提交，包含7大功能页面,三套开源项目和前端视觉规范文档

---
+++ first_demo.py
@@ -0,0 +1,4 @@
+print("study_sys 环境运行成功！")
+word_list = ["错题", "试卷", "单词"]
+for w in word_list:
+    print("待处理内容：", w)
---
+++ frontend/qwerty-learner/src/index.tsx
@@ -0,0 +1,72 @@
+import Loading from './components/Loading'
+import './index.css'
+import { ErrorBook } from './pages/ErrorBook'
+
+import MobilePage from './pages/Mobile'
+import TypingPage from './pages/Typing'
+import { isOpenDarkModeAtom } from '@/store'
+
+import 'animate.css'
+import { useAtomValue } from 'jotai'
+
+import process from 'process'
+import React, { Suspense, lazy, useEffect, useState } from 'react'
+import 'react-app-polyfill/stable'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+
+const AnalysisPage = lazy(() => import('./pages/Analysis'))
+
+
+
+
+function Root() {
+  const darkMode = useAtomValue(isOpenDarkModeAtom)
+  useEffect(() => {
+    darkMode ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark')
+  }, [darkMode])
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 600)
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isMobile = window.innerWidth <= 600
+      if (!isMobile) {
+        window.location.href = '/'
+      }
+      setIsMobile(isMobile)
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return (
+    <React.StrictMode>
+      <BrowserRouter basename={REACT_APP_DEPLOY_ENV === 'pages' ? '/qwerty-learner' : ''}>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            {isMobile ? (
+              <Route path="/*" element={<Navigate to="/mobile" />} />
+            ) : (
+              <>
+                <Route index element={<TypingPage />} />
+                
+                <Route path="/analysis" element={<AnalysisPage />} />
+                <Route path="/error-book" element={<ErrorBook />} />
+                
+                <Route path="/*" element={<Navigate to="/" />} />
+              </>
+            )}
+            <Route path="/mobile" element={<MobilePage />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+      
+    </React.StrictMode>
+  )
+}
+
+const container = document.getElementById('root')
+
+container && createRoot(container).render(<Root />)
